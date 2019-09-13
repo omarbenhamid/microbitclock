92445fa759f735617acc077545e2c816e89ad29e
Omar BENHAMID <omarbenhamid@users.noreply.github.com>
2019-09-13 12:22:14 +0200

    WIP Single datafile mode

--- main.js
+++ main.js
@@ -7,47 +7,54 @@
 // will create st-MMDD.txt with the data sent until $
 //
 // C is for chourouk
+//
+// type #$ to cancel line completely
 serial.onDataReceived(serial.delimiters(Delimiters.Dollar), function () {
     basic.showIcon(IconNames.TShirt)
-    dataline = serial.readUntil(serial.delimiters(Delimiters.Dollar))
-    let data = dataline.split(',')
-    serial.writeString("DATA:")
+    let dataline: string = serial.readUntil(serial.delimiters(Delimiters.Dollar))
+    if (dataline.indexOf('#') >= 0) {
+        serial.writeString("\r\n Cancel line")
+        return;
+    }
+    if (dataline.length == 0) {
+        serial.writeLine("Dumping salat times file")
+        f.seek(0, FileSystemSeekFlags.Set)
+        let b: Buffer = null;
+        do {
+            b = f.readBuffer(64)
+            serial.writeBuffer(b)
+        } while (b.length > 0)
+        serial.writeLine("\r\n done")
+        return;
+    }
+
+    while (dataline.length > 0 && "\n\r\t ".indexOf(dataline[0]) >= 0)
+        dataline = dataline.substr(1)
+
+    if (dataline.length != 41) {
+        serial.writeLine("Bad format : line must be 41 characters, " + dataline.length + " found")
+        return;
+    }
+
+    serial.writeString("\r\nProcessing:")
     serial.writeLine(dataline)
-    if (data.length == 1) {
-        m = parseInt(data[0])
-        serial.writeLine("Dump calender of month " + m)
-        let jd = jdm(m)
-        for (j = 1; j <= jd; j++) {
-            files.readToSerial("st-" + toTwoDigitText(m) + toTwoDigitText(j) + ".txt")
-        }
-        serial.writeLine("" + "\r\nDone")
-        return;
-    }
-    if (data.length == 2) {
-        n = parseInt(data[0])
-        j = parseInt(data[1])
-        serial.writeLine("Dump calender of day " + j + "/" + n)
-        files.readToSerial("st-" + toTwoDigitText(n) + toTwoDigitText(j) + ".txt")
-        serial.writeLine("" + "\r\nDone")
-        return;
-    }
+
+    let data = dataline.split(',')
+
     if (data.length != 8) {
         serial.writeLine("Bad format: 8 columns required")
         return;
     }
-    fname = "st-"
     j = parseInt(data[0])
     if (!(j >= 1 && j <= 12)) {
         serial.writeLine("Bad format: Col 1 (month) must be a number between 1 and 12")
         return;
     }
-    fname = "" + fname + toTwoDigitText(j)
     j = parseInt(data[1])
     if (!(j >= 1 && j <= 31)) {
         serial.writeLine("Bad format: Col 2 (day) must be a number between 1 and 31")
         return;
     }
-    fname = "" + fname + toTwoDigitText(j) + ".txt"
     data.forEach(function (v: string, idx: number) {
         if (idx < 2) return; //Skip month / day
         if (v.indexOf(':') < 0) {
@@ -70,58 +77,60 @@ serial.onDataReceived(serial.delimiters(Delimiters.Dollar), function () {
     if (fail) {
         return;
     }
-    serial.writeLine("Updating " + fname)
-    f = files.open(fname)
-    serial.writeLine("Openend")
-    f.close()
-    serial.writeLine("Cloased")
-    f.remove()
-    serial.writeLine("Removed")
-    basic.pause(500)
-    f.open()
-    serial.writeLine("Opened again")
-    f.writeString(dataline)
-    serial.writeLine("Wrote")
+    serial.writeLine("Updating salat times")
+    f.seek(0, FileSystemSeekFlags.End)
+    f.writeString(dataline + "\r\n")
     f.flush()
-    serial.writeLine("Flushed")
-    f.close()
-    serial.writeLine("Closing")
     nextSalat = -1
-    serial.writeLine("" + "\r\nDone")
+    serial.writeLine("Done")
     basic.showIcon(IconNames.Yes)
 })
-function loadNextSalat(mois: number, jour: number, heureAct: number, minAct: number, checkNextDay: boolean) {
+function loadNextSalat(mois: number, jour: number, heureAct: number, minAct: number) {
     basic.showIcon(IconNames.SmallDiamond)
-    files.readToSerial("st-" + toTwoDigitText(mois) + toTwoDigitText(jour) + ".txt")
     serial.writeLine("\r\nComputing next salat time.")
-    f = files.open("st-" + toTwoDigitText(mois) + toTwoDigitText(jour) + ".txt")
-    b = f.readBuffer(64).toString()
-    f.close()
-    b.split(',').find(function (v: string, i: number): boolean {
-        if (i < 2) return false; //Skip month / day
-        let p = v.split(':');
-        nextSalatHour = parseFloat(p[0]);
-        nextSalatMin = parseFloat(p[1]);
-        if (nextSalatHour < heureAct) return false;
-        if (nextSalatHour == heureAct && nextSalatMin <= minAct) return false;
-        nextSalat = i - 1;
-        debug("Next", nextSalatHour + ":" + nextSalatMin, " index :" + nextSalat);
-        return true;
-    });
-    if (nextSalat < 0 && checkNextDay) {
-        debug("next day")
-        if (jour < jdm(mois)) {
-            jour = jour + 1
-        } else {
-            jour = 1
-            mois = mois + 1
-            if (mois > 12) {
-                mois = 1
+    f.seek(0, FileSystemSeekFlags.Set)
+    b = "haha!"
+    let nextDay = false;
+    while (nextSalat < 0) {
+        b = f.readBuffer(43).toString();
+        debug(b)
+        if (b.length < 43) {
+            //End of file
+            if (nextDay) {
+                serial.writeLine("Failed finding a salat time")
+                basic.showIcon(IconNames.No)
+                return;
+            } else {
+                serial.writeLine("Checking tomorrow")
+                nextDay = true;
+                if (jour < jdm(mois)) {
+                    jour = jour + 1
+                } else {
+                    jour = 1
+                    mois = mois + 1
+                    if (mois > 12) {
+                        mois = 1
+                    }
+                }
+                f.seek(0, FileSystemSeekFlags.Set)
+                continue;
             }
         }
-        debug(jour + "/" + mois)
-        loadNextSalat(mois, jour, 0, 0, false)
+        let data = b.split(',');
+        if (parseInt(data[0]) != mois || parseInt(data[1]) != jour) continue;
+        data.find(function (v: string, i: number): boolean {
+            if (i < 2) return false; //Skip month / day
+            let p = v.split(':');
+            nextSalatHour = parseFloat(p[0]);
+            nextSalatMin = parseFloat(p[1]);
+            if (nextSalatHour < heureAct) return false;
+            if (nextSalatHour == heureAct && nextSalatMin <= minAct) return false;
+            nextSalat = i - 1;
+            debug("Next", nextSalatHour + ":" + nextSalatMin, " index :" + nextSalat);
+            return true;
+        });
     }
+
     basic.showIcon(IconNames.Yes)
 }
 function checkAdhan(h: number, m: number) {
@@ -167,25 +176,24 @@ input.onButtonPressed(Button.A, function () {
 
 })
 let nacc = 0
-let fname = ""
-let n = 0
-let m = 0
 let sleepTimeOutMillis = 0
 let acc = 0
 let wakeUpTime = 0
-let mois = 0
-let jour = 0
-let ds: DS1302.DS1302RTC = null
-let j = 0
-let dataline = ""
-let nextSalatMin = 0
-let nextSalatHour = 0
-let b = ""
 let f: files.File = null
-let nextSalat = 0
-let fname2 = ""
-let fail = false
 let mois31: number[] = []
+let fail = false
+let nextSalat = 0
+let b = ""
+let nextSalatHour = 0
+f = files.open("salatimes.txt")
+let nextSalatMin = 0
+let dataline = ""
+let j = 0
+let ds: DS1302.DS1302RTC = null
+let jour = 0
+let mois = 0
+let m = 0
+let n = 0
 function jdm(mois: number): number {
     if (mois == 2) {
         if (ds.getYear() % 4 == 0) {
@@ -199,10 +207,10 @@ function jdm(mois: number): number {
     return 30
 }
 function debug(message: string, message2: string = null, message3: string = null) {
-    let m = message;
-    if (message2) m = m + message2;
-    if (message3) m = m + message3;
-    serial.writeLine(m)
+    let o = message;
+    if (message2) o = o + message2;
+    if (message3) o = o + message3;
+    serial.writeLine(o)
 }
 mois31 = [1, 3, 5, 7, 8, 10, 12]
 serial.redirectToUSB()
@@ -225,7 +233,7 @@ basic.forever(function () {
     checkAdhan(ds.getHour(), ds.getMinute())
     updateSleepState()
     if (nextSalat < 0) {
-        loadNextSalat(ds.getMonth(), ds.getDay(), ds.getHour(), ds.getMinute(), true)
+        loadNextSalat(ds.getMonth(), ds.getDay(), ds.getHour(), ds.getMinute())
     }
     if (wakeUpTime >= 0) {
         OLED12864_I2C.showString(
